78f18a97f07d0daf4e0f51da6f93d79f8c0a2f0d
Joel Chan <cywjoel@gmail.com>
2023-11-19 18:46:11 +0800

    Add credit card; add about footer; test dark sticky header (contact.html)

--- scripts/displayPaymentSummary.js
+++ scripts/displayPaymentSummary.js
@@ -29,8 +29,8 @@ for (let item in cartArray) {
 document.write('</div>');
 
 document.write('<div class="col-12 col-lg-4 my-auto">');
-document.write('<div class="mb-4">');
 document.write('<form id="payment" method="post">');
+document.write('<div class="mb-4">');
 document.write('<label for="payment-method" class="form-label text-white">Payment method</label>');
 document.write('<select id="paymentMethod" name="payment-method" class="form-select" placeholder="Confirm your country" aria-label="Payment method" required>');
 document.write('<option value="" disabled selected>Select</option>');
@@ -38,13 +38,34 @@ document.write('<option value="credit-card">Credit Card</option>');
 document.write('<option value="debit-card">Debit Card</option>');
 document.write('</select>');
 document.write('</div>');
+
+document.write('<div class="form-group my-2">');
+document.write('<label style="font-family:\'DM Sans\', sans-serif" class="text-white" for="cardName">Name on Card</label>');
+document.write('<input type="text" style="font-family:\'DM Sans\', sans-serif" class="form-control" id="cardName" placeholder="Name on Card" required>');
+document.write('<br />');
+document.write('<label class="text-white" for="cardNumber">Card Number</label>');
+document.write('<input type="text" style="font-family:\'DM Sans\', sans-serif" class="form-control" id="cardNumber" placeholder="Card Number" required>');
+document.write('<br />');
+document.write('<div class="row">');
+document.write('<div class="col-7">');
+document.write('<label class="text-white" for="expiryDate">Expiry Date</label>');
+document.write('<input type="text" style="font-family:\'DM Sans\', sans-serif" class="form-control" id="expiryDate" placeholder="MM/YY" required>');
+document.write('</div>')
+document.write('<div class="col-5">');
+document.write('<label class="text-white" for="cvv">CVV</label>');
+document.write('<input type="text" style="font-family:\'DM Sans\', sans-serif" class="form-control" id="cvv" placeholder="CVV" required>');
+document.write('</div>')
+document.write('</div>');
+document.write('</div>');
+
 document.write('<div class="mt-4 text-center text-lg-start">');
 document.write('<h4>Total: S$' + (totalCost + deliveryFee).toFixed(2) + '</h4>');
 document.write('</div>');
-document.write('<button class="btn btn-secondary my-3" type="submit" id="makePayment" style="width: 100%;">Proceed to Pay</button>');
+
+document.write('<button class="btn btn-secondary mb-3" type="submit" id="makePayment" style="width: 100%;">Proceed to Pay</button>');
 document.write('<div class="form-check">');
 document.write('<input class="form-check-input" type="checkbox" name="pp-agree" value="1" required>');
 document.write('<label class="form-check-label text-white" for="pp-agree">By clicking on this, you are agreeing to the <a class="text-white" href="privacy.html">Privacy Policy</a>.</label>');
-document.write('</form>');
 document.write('</div>');
+document.write('</form>');
 document.write('</div>');
